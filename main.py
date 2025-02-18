1fda5a01b974bd274e0a4ff4e83003f85ebf5ae7
Basil Vazhathottathil <basilveldose@gmail.com>
2025-02-19 00:06:16 +0530

    A1;got article links from rss feed

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+import os
+import requests
+from bs4 import BeautifulSoup
+
+src= r'D:\python_elec\news\src.json'
+
+def main():
+    if os.path.exists(src):
+        with open(src,'r') as file:
+            data=json.load(file)
+            for key,url in data.items():
+                try:
+                    response=requests.get(url)
+                    response.raise_for_status()
+                    soup=BeautifulSoup(response.text,'xml')
+                    get_links(soup)
+                except requests.exceptions.RequestException as e:
+                    print('unable to fetch url for {key}:',e)
+    else:
+        print('the file is not at given address')
+
+
+def get_links(soup):
+    links_in_rss= soup.find_all('link')
+    for link in links_in_rss:
+        print(link.get_text())
+
+
+main()
