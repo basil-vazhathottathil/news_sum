8f08db605b6aec93fe7c9b7cc713d2994adb3e93
Basil Vazhathottathil <basilveldose@gmail.com>
2025-03-04 18:14:23 +0530

    still dont know shit T_T

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -4,15 +4,15 @@ const Card = ({title,summary,link,index}) => {
   return (
     <div className='p-4 bg-gray-300 h-100 w-150 shadow-md rounded-lg flex flex-col justify-between'>
       <div>
-        <h2 className='text-xl text-center text-gray-900 font-bold mb-2'>{title}</h2>
-        <p className='text-gray-600 mb-4 text-center'>{summary}</p>
+        <h2 className='text-xl text-center text-gray-900 font-bold mb-2'>Title</h2>
+        <p className='text-gray-600 mb-4 text-center'>this is where the summary is going to be at</p>
       </div>
       <div className='flex flex-col items-center'>
         
         <div className='flex space-x-30 justify-center'>
-          <button  onClick={index--} className='w-24 px-4 py-2 bg-blue-500 text-white rounded'>Previous</button>
-          <a href={link} className='text-blue-500 mb-4 block'>src article</a>
-          <button  onClick={index++} className='w-24 px-4 py-2 bg-gray-500 text-white rounded'>Next</button>
+          <button  className='w-24 px-4 py-2 bg-blue-500 text-white rounded'>Previous</button>
+          <a href= '/' className='text-blue-500 mb-4 block'>src article</a>
+          <button   className='w-24 px-4 py-2 bg-gray-500 text-white rounded'>Next</button>
         </div>
       </div>
     </div>
