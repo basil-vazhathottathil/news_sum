5d2f000a5ffebe79c593604660b18c227aabab6c
Basil Vazhathottathil <basilveldose@gmail.com>
2025-03-10 14:09:03 +0530

    well well well

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/card'
+import getData from './functions/app'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const result= await getData();
+      setData(result);
+    }
+    fetchData();
+  }, []);
+  
+
 
   return (
     <>
       <div className="flex items-center justify-center rounded-lg w-screen h-screen ">
-        <Card/>
+        {data.map((item,index)=> (
+          <Card key={index} title={item.title} summary={item.summary} link={item.link}/>
+        ))}
         </div>
     </>
   )
--- vite-project/src/functions/app.js
+++ vite-project/src/functions/app.js
@@ -2,7 +2,7 @@ async function getData(){
     try{
         const response= await fetch('http://127.0.0.1:3000/summaries');
 
-        const data= response.json();
+        const data=  await response.json();
         console.log(data);
         return data.summaries;
         
